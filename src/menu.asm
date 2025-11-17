1019be2387184f8870191b3c473c5a9a32c24375
swisd <glordi053@gmail.com>
2025-11-17 11:04:14 -0500

    Initial commit

---
+++ src/menu.asm
@@ -0,0 +1,4 @@
+
+;   P   Y   W      Project/Synth/Wave
+;   S C P I T      Song/Chain/Phrase/Inst/Track
+;   G              Groove
---
+++ src/oscilloscope.asm
@@ -0,0 +1,11 @@
+;    5 x 15 chars
+;    set="|/-_\~"
+
+OSC_CWIDTH    DB   15
+OSC_CHEIGHT   DB   5
+
+OSC_FPS       DW   32
+
+;
+;
+;
